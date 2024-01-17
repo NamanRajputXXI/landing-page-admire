b7ca010c9b3df860a3f96d872702dec68fe5fecc
Naman Rajput <namanrajput801@gmail.com>
2024-01-17 11:08:39 +0530

    ui:fix the hero section image and complete the popular desitination section

--- src/components/PopularDestination/PopularDestination.jsx
+++ src/components/PopularDestination/PopularDestination.jsx
@@ -5,20 +5,45 @@ const PopularDestination = () => {
   return (
     <section className="flex flex-col items-center justify-center py-20 gap-20 px-4 max-w-7xl mx-auto">
       <div className="w-full flex sm:flex-row gap-5 flex-col justify-between">
-        <div className="flex flex-col  gap-1">
-          <h1 className="sm:text-2xl text-xl font-bold">Popular Destination</h1>
-          <p className="text-gray-600 text-base">
-            Recommendation Places for you
-          </p>
+        <div className="flex flex-col  gap-2">
+          <h1 className="sm:text-5xl text-xl font-bold">Popular Destination</h1>
+          <p className="text-gray-600 text-xl">Recommendation Places for you</p>
         </div>
         <button className="bg-[#FD4A4C] h-12 rounded-xl w-44 px-3 py-1 flex items-center justify-center text-white">
           See all Destination
         </button>
       </div>
       <div className="grid md:grid-cols-2 items-center w-fit justify-center gap-10 grid-cols-1 lg:grid-cols-3">
-        <PopularDestinationCard />
-        <PopularDestinationCard />
-        <PopularDestinationCard />
+        <PopularDestinationCard
+          imgUrl={
+            "https://images.unsplash.com/photo-1591797762236-77dd6dcaa956?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          heading={"Beach Walk, Badung"}
+          location={"Bali, Indonasia"}
+          rating={"8.4/10"}
+          reviews={"(536 Reviews)"}
+          cost={"₹5000"}
+        />
+        <PopularDestinationCard
+          imgUrl={
+            "https://images.unsplash.com/photo-1605537964076-3cb0ea2ff329?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHBhcmFkaXNlfGVufDB8fDB8fHww"
+          }
+          heading={"The Singhasari royal hotel"}
+          location={"Jakarta, Indonasia"}
+          rating={"9.2/10"}
+          reviews={"(758 Reviews)"}
+          cost={"₹3000"}
+        />
+        <PopularDestinationCard
+          imgUrl={
+            "https://images.unsplash.com/photo-1563911302283-d2bc129e7570?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhvdGVsfGVufDB8fDB8fHww"
+          }
+          heading={"Paradise Luxury Hotel"}
+          location={"Maland, Indonasia"}
+          rating={"9.0/10"}
+          reviews={"(977 Reviews)"}
+          cost={"₹8000"}
+        />
       </div>
     </section>
   );
--- src/components/PopularDestination/PopularDestinationCard.jsx
+++ src/components/PopularDestination/PopularDestinationCard.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa";
-const PopularDestinationCard = () => {
+const PopularDestinationCard = ({
+  imgUrl,
+  heading,
+  location,
+  rating,
+  reviews,
+  cost,
+}) => {
   return (
-    <div className="flex shadow-2xl  justify-center items-center w-fit h-fit rounded-b-lg flex-col  ">
+    <div className="flex shadow-xl w-full justify-center rounded-b-lg flex-col  ">
       <img
-        src="https://images.unsplash.com/photo-1506929562872-bb421503ef21?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGJlYWNofGVufDB8fDB8fHww"
+        src={imgUrl}
         alt=""
-        className="h-[350px] w-[350px] rounded-t-lg"
+        className=" w-full sm:h-[400px] h-[300px]  rounded-t-lg"
       />
-      <div className="flex flex-col py-4 px-4 gap-4">
-        <h5 className="text-xl font-semibold">Beach Walk Badung</h5>
+      <div className="flex px-5 flex-col py-4 gap-4">
+        <h5 className="text-xl font-semibold">{heading}</h5>
         <div className="flex gap-2 items-center">
-          <FaLocationDot /> Bali, Indonasia
+          <FaLocationDot /> {location}
         </div>
         <div className="flex items-center gap-2">
           <div className="flex items-center gap-1">
@@ -22,13 +29,13 @@ const PopularDestinationCard = () => {
             <FaStar color="#FFD700" />
             <FaStar color="#FFD700" />
           </div>
-          <div className="flex items-center gap-1">
-            <p>8.4/10</p>
-            <p className="text-gray-500">(536 Reviews)</p>
+          <div className="flex font-medium items-center gap-1">
+            <p className=" sm:text-base text-sm">{rating}</p>
+            <p className="text-gray-500  sm:text-base text-sm">{reviews}</p>
           </div>
         </div>
-        <div className="flex items-center text-gray-500">
-          <h5>₹ 5000</h5>/night
+        <div className="flex font-medium sm:text-base text-sm items-center text-gray-500">
+          <h5>{cost}</h5>/night
         </div>
         <button className="bg-[#FD4A4C] px-4 py-2 flex items-center w-44 rounded-lg text-white justify-center">
           Explore
